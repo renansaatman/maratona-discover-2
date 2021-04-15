f823a7aa11bcf963328950d78db37e43071ea6a2
Renan Saatman <renansaatman@hotmail.com>
2021-04-15 18:51:20 -0300

    create controllers/model/utils

---
+++ src/controllers/DashboardController.js
@@ -0,0 +1,42 @@
+const Job = require('../model/Job')
+const Profile = require('../model/Profile')
+const JobUtils = require('../Utils/JobUtils')
+
+module.exports = {
+    index(req, res) {
+        const jobs = Job.get()
+        const profile = Profile.get()
+
+        let statusCount = {
+            progress: 0,
+            done: 0,
+            total: jobs.length
+        }
+
+        let jobTotalHours = 0
+
+        const updatedJobs = jobs.map((job) => {
+    
+            const remaining = JobUtils.remainingDays(job)
+            const status = remaining <= 0 ? 'done' : 'progress'
+            
+            // quando o status muda para done ou progress ele conta 1 na respectiva variavel
+            statusCount[status] += 1
+
+            // total de horas por job ainda em andamento
+            jobTotalHours = status === 'progress' ? jobTotalHours + Number(job['daily-hours']) : jobTotalHours
+    
+            return {
+                ...job,
+                remaining,
+                status,
+                budget: JobUtils.calculateBudget(job, profile["value-hour"])
+            }
+        })
+        
+        //qtd de horas/dia que quero trabalhar - qtd de horas/dia de cada job em andamento
+        const freeHours = profile['hours-per-day'] - jobTotalHours
+        return res.render("index", { jobs: updatedJobs, profile, statusCount, freeHours })
+    },
+}
+
---
+++ src/utils/JobUtils.js
@@ -0,0 +1,22 @@
+module.exports = {
+    remainingDays(job) {
+        //ajustes no job
+        //calculo de tempo restante
+
+        const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+
+        const createdDate = new Date(job.created_at) //dia que o projeto foi criado
+        const dueDay = createdDate.getDate() + Number(remainingDays)
+        const dueDateInMs = createdDate.setDate(dueDay)
+
+        const timeDiffInMs = dueDateInMs - Date.now()
+        //transformar ms em dias
+        const dayInMs = 1000 * 60 * 60 * 24
+        const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+
+        //restam x dias
+        return dayDiff
+    },
+
+    calculateBudget: (job, valueHour) => valueHour * job["total-hours"]
+}
