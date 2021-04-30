7cd754996ca16a8e9add2d778e23dc04fc23f9f4
Renan Saatman <renansaatman@hotmail.com>
2021-04-30 16:16:52 -0300

    fix Math.floor calculation

--- src/utils/JobUtils.js
+++ src/utils/JobUtils.js
@@ -12,7 +12,7 @@ module.exports = {
         const timeDiffInMs = dueDateInMs - Date.now()
         //transformar ms em dias
         const dayInMs = 1000 * 60 * 60 * 24
-        const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+        const dayDiff = Math.ceil(timeDiffInMs / dayInMs)
 
         //restam x dias
         return dayDiff
